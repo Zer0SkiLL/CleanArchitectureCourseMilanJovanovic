a81791c4473eb6efcb80cbce8ce49456e99b57ca
Widmer Reto <reto.widmer@finstar.ch>
2024-09-16 12:27:43 +0200

    add serilog structured logging

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,8 +1,14 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddControllers();
 
@@ -26,6 +32,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandler();
 
