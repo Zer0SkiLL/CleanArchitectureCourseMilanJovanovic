01749aa4010c240ba1406e13b86eb20dfae9b3f6
Widmer Reto <reto.widmer@finstar.ch>
2024-09-16 07:58:01 +0200

    initial code push end of chapter 7 Athorization

---
+++ src/Bookify.Domain/Users/Role.cs
@@ -0,0 +1,16 @@
+namespace Bookify.Domain.Users;
+
+public sealed class Role
+{
+    public static readonly Role Registered = new(1, "Registered");
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public ICollection<User> Users { get; init; } = new List<User>();
+    public ICollection<Permission> Permissions { get; init; } = new List<Permission>();
+
+    public Role(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
