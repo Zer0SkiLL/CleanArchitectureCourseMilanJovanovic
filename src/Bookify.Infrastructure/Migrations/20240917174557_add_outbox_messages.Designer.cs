101ba407b5906abc505487c138248df796d377bb
Widmer Reto <reto.widmer@finstar.ch>
2024-09-18 09:06:40 +0200

    outbox messages and processor with quartz

--- src/Bookify.Infrastructure/Migrations/20240917174557_add_outbox_messages.Designer.cs
+++ src/Bookify.Infrastructure/Migrations/20240917174557_add_outbox_messages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bookify.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bookify.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240917174557_add_outbox_messages")]
+    partial class add_outbox_messages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -305,9 +308,9 @@ namespace Bookify.Infrastructure.Migrations
                         .HasColumnType("text")
                         .HasColumnName("error");
 
-                    b.Property<DateTime>("OccurredOnUtc")
+                    b.Property<DateTime>("OccuredOnUtc")
                         .HasColumnType("timestamp with time zone")
-                        .HasColumnName("occurred_on_utc");
+                        .HasColumnName("occured_on_utc");
 
                     b.Property<DateTime?>("ProcessedOnUtc")
                         .HasColumnType("timestamp with time zone")
---
+++ src/Bookify.Infrastructure/Migrations/20240918053816_outbox_typo_fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class outbox_typo_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "occured_on_utc",
+                table: "outbox_messages",
+                newName: "occurred_on_utc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "occurred_on_utc",
+                table: "outbox_messages",
+                newName: "occured_on_utc");
+        }
+    }
+}
